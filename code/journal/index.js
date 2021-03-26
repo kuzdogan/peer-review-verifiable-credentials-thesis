3447ea92f3be6d82854a42559d1de69f81f326d4
kuzdogan <kaanuzdogan@hotmail.com>
2021-03-26 14:44:24 +0300

    Cache contexts, wrap vc issueance in function

--- code/journal/index.js
+++ code/journal/index.js
@@ -3,18 +3,28 @@ const vc = require('vc-js');
 const keyPairOptions = require('./keypair.json')
 const { extendContextLoader } = require('jsonld-signatures');
 const { documentLoaders } = require("jsonld");
-const peerReviewSchema = require('../PeerReview.json')
+const peerReviewSchema = require('../PeerReview.json');
+const examplePR = require('../ExamplePeerReview.json');
+
+// cached contexts
+const securityContexts = require('jsonld-signatures/lib/contexts');
+const vc_v1 = require('../contexts/vc-v1.json');
+const security_v3_unstable = require('../contexts/security-v3-unstable.json');
 
 _main()
 
 const documents = {
-  "https://raw.githubusercontent.com/kuzdogan/peer-review-verifiable-credentials-thesis/main/code/PeerReviewVC.jsonld": peerReviewSchema
+  "https://raw.githubusercontent.com/kuzdogan/peer-review-verifiable-credentials-thesis/main/code/PeerReview.json": peerReviewSchema,
+  'https://www.w3.org/2018/credentials/v1': vc_v1,
+  ...securityContexts,
+  'https://w3id.org/security/v3-unstable': security_v3_unstable
 };
 
 const customDocLoader = (url) => {
   const context = documents[url];
 
   if (context) {
+    console.log(`Found cached ${url}, not requesting`)
     return {
       contextUrl: null, // this is for a context via a link header
       document: context, // this is the actual document that was loaded
@@ -27,56 +37,22 @@ const documentLoader = extendContextLoader(customDocLoader);
 
 
 async function _main() {
-
-  // Sample unsigned credential
-  const credential = {
-    "@context": [
-      "https://www.w3.org/2018/credentials/v1",
-      "https://raw.githubusercontent.com/kuzdogan/peer-review-verifiable-credentials-thesis/main/code/PeerReviewVC.jsonld"
-    ],
-    "id": "https://peerreview.org/reviews/1234",
-    "type": [
-      "VerifiableCredential",
-      "PeerReviewCredential"
-    ],
-    "description": "Peer Review Credential for the article Tourette syndrome research highlights from 2018",
-    "issuer": "https://f1000research.org/.well-known/did.jsonld",
-    "issuanceDate": "2017-12-05T14:27:42Z",
-    "credentialSubject": {
-      "id": "doi:10.1000/182",
-      "type": "PeerReview",
-      "name": "Peer Review: Tourette syndrome research highlights from 2018",
-      "author": {
-        "type": "PeerReviewAuthor",
-        "id": "orcid:0000-0002-3037-3890",
-        "givenName": "Marc",
-        "familyName": "Lavoie",
-        "email": "marc@lavoie.net"
-      }
-    }
+  try {
+    let signedVC = await issuePeerReview(examplePR);
+    console.log(signedVC);
+  } catch (err) {
+    console.log('Error issuing credential');
+    console.error(err);
   }
-
-
-
-  const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
-  console.log('Generated KeyPair');
-  // console.log(keyPair);
-  const suite = new BbsBlsSignature2020({ key: keyPair });
-  console.log('Generated key suite');
-  // console.log(suite);
-  const signedVC = await vc.issue({ credential, suite, documentLoader });
-  console.log(JSON.stringify(signedVC, null, 2));
 }
 
 
 
-// function issuePeerReview(reviewDoc) {
-//   //Import the example key pair
-//   const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
-//   //Sign the input document
-//   const signedDocument = await sign(inputDocument, {
-//     suite: new BbsBlsSignature2020({ key: keyPair }),
-//     purpose: new purposes.AssertionProofPurpose(),
-//     documentLoader
-//   });
-// }
+async function issuePeerReview(unsignedCredential) {
+  const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
+  console.log('Generated KeyPair');
+  const suite = new BbsBlsSignature2020({ key: keyPair });
+  console.log('Generated key suite');
+  const signedVC = await vc.issue({ credential: unsignedCredential, suite, documentLoader });
+  return signedVC;
+}
