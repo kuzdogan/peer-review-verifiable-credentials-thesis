c6cb0c16f2387f0a0235c63a140ab00c95d834de
kuzdogan <kaanuzdogan@hotmail.com>
2021-03-26 19:03:16 +0300

    Verify credential with BBS
    Add cached bbs2020 context. Add bbs context to the example PR
    credential. Change the dummy issuer in the PR credential to an example
    did that can resolve to a document with keys.

--- code/journal/index.js
+++ code/journal/index.js
@@ -1,27 +1,32 @@
 const { BbsBlsSignature2020, Bls12381G2KeyPair } = require("@mattrglobal/jsonld-signatures-bbs");
-const vc = require('vc-js');
-const keyPairOptions = require('./keypair.json')
-const { extendContextLoader } = require('jsonld-signatures');
+const { extendContextLoader, purposes, verify, sign } = require('jsonld-signatures');
 const { documentLoaders } = require("jsonld");
-const peerReviewSchema = require('../PeerReview.json');
+const keyPairOptions = require('./keypair.json')
 const examplePR = require('../ExamplePeerReview.json');
+const exampleControllerDoc = require("./controllerDocument.json");
 
 // cached contexts
+const peerReviewSchema = require('../PeerReview.json');
 const securityContexts = require('jsonld-signatures/lib/contexts');
 const vc_v1 = require('../contexts/vc-v1.json');
 const security_v3_unstable = require('../contexts/security-v3-unstable.json');
+const bbsContext = require("../contexts/bbs.json");
 
 _main()
 
-const documents = {
+// cached contexts
+const contexts = {
+  "did:example:489398593#test": keyPairOptions,
+  "did:example:489398593": exampleControllerDoc,
   "https://raw.githubusercontent.com/kuzdogan/peer-review-verifiable-credentials-thesis/main/code/PeerReview.json": peerReviewSchema,
   'https://www.w3.org/2018/credentials/v1': vc_v1,
-  ...securityContexts,
-  'https://w3id.org/security/v3-unstable': security_v3_unstable
+  ...securityContexts, // security-v1, security-v2
+  'https://w3id.org/security/v3-unstable': security_v3_unstable,
+  "https://w3id.org/security/bbs/v1": bbsContext,
 };
 
 const customDocLoader = (url) => {
-  const context = documents[url];
+  const context = contexts[url];
 
   if (context) {
     console.log(`Found cached ${url}, not requesting`)
@@ -37,22 +42,42 @@ const documentLoader = extendContextLoader(customDocLoader);
 
 
 async function _main() {
+  let signedVC, isVerified
   try {
-    let signedVC = await issuePeerReview(examplePR);
+    signedVC = await issuePeerReview(examplePR);
     console.log(signedVC);
   } catch (err) {
     console.log('Error issuing credential');
     console.error(err);
   }
+  try {
+    isVerified = await verifyPeerReview(signedVC);
+    console.log('Verified: ', isVerified);
+  } catch (err) {
+    console.log('Error verifying credential');
+    console.error(err);
+  }
 }
 
-
+async function verifyPeerReview(signedCredential) {
+  console.log('Verifying Credential')
+  let isVerified = await verify(signedCredential, {
+    suite: new BbsBlsSignature2020(),
+    purpose: new purposes.AssertionProofPurpose(),
+    documentLoader
+  });
+  return isVerified;
+}
 
 async function issuePeerReview(unsignedCredential) {
   const keyPair = await new Bls12381G2KeyPair(keyPairOptions);
   console.log('Generated KeyPair');
   const suite = new BbsBlsSignature2020({ key: keyPair });
   console.log('Generated key suite');
-  const signedVC = await vc.issue({ credential: unsignedCredential, suite, documentLoader });
+  const signedVC = await sign(unsignedCredential, {
+    purpose: new purposes.AssertionProofPurpose(),
+    suite,
+    documentLoader
+  });
   return signedVC;
 }
