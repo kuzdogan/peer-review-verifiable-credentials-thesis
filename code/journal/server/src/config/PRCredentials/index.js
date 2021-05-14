de004f6ce6e66bf34660628cd87aa894c59bb043
kuzdogan <kaanuzdogan@hotmail.com>
2021-05-14 18:37:36 +0300

    Update jsonld & jsonld-signatures versions on journal

--- code/journal/server/src/config/PRCredentials/index.js
+++ code/journal/server/src/config/PRCredentials/index.js
@@ -18,13 +18,14 @@ const vc_v1 = require('./contexts/vc-v1.json');
 const security_v3_unstable = require('./contexts/security-v3-unstable.json');
 const bbsContext = require('./contexts/bbs.json');
 
-_main();
+// _main();
 
 // cached contexts
 const contexts = {
   'did:example:489398593#test': keyPairOptions,
   'did:example:489398593': exampleControllerDoc,
-  'https://raw.githubusercontent.com/kuzdogan/peer-review-verifiable-credentials-thesis/main/code/PeerReview.json': peerReviewSchema,
+  'https://raw.githubusercontent.com/kuzdogan/peer-review-verifiable-credentials-thesis/main/code/PeerReview.json':
+    peerReviewSchema,
   'https://www.w3.org/2018/credentials/v1': vc_v1,
   ...securityContexts, // security-v1, security-v2
   'https://w3id.org/security/v3-unstable': security_v3_unstable,
@@ -95,11 +96,52 @@ async function selectiveDisclose(signedDocument, revealDocument) {
   return derivedProof;
 }
 
+const temp = {
+  '@context': [
+    'https://www.w3.org/2018/credentials/v1',
+    'https://raw.githubusercontent.com/kuzdogan/peer-review-verifiable-credentials-thesis/main/code/PeerReview.json',
+    'https://w3id.org/security/bbs/v1',
+  ],
+  id: 'http://localhost:3000/reviews/609ac62276bfdce844d8ee6c/credential',
+  type: ['PeerReviewCredential', 'VerifiableCredential'],
+  description: 'A Verifiable Credential representing a peer review that is done for a scholarly article.',
+  name: 'Peer Review Credential version 0.1',
+  credentialSubject: {
+    id: 'http://localhost:3000/users/609ac62276bfdce844d8ee6c',
+    type: 'PeerReview',
+    competingInterestStatement: 'No competing interests',
+    author: {
+      id: 'orcid:0000-0001-7223-2669',
+      type: 'PeerReviewAuthor',
+      email: 'alice@test.com',
+    },
+    journal: {
+      id: 'http://localhost:3000',
+      issn: '2046-1402',
+      name: 'International Journal of X',
+    },
+  },
+  issuanceDate: '2021-05-11T23:28:18.849Z',
+  issuer: 'did:example:489398593',
+  proof: {
+    type: 'BbsBlsSignatureProof2020',
+    created: '2021-05-11T23:28:18Z',
+    nonce: 'NxeHm6Dx0rEjVf8LguPDssE55ZhQryDnN9sbwG6Famiq/Jq018/Vye9dNlmQmKFmJpM=',
+    proofPurpose: 'assertionMethod',
+    proofValue:
+      'ABwJbj/PtfHJmdtKn1wO2EwiYtNGvSuVJoW0JtNYORi2fTU0Ydd+JzT49mAteNzAkLcwjRxZrqM9rAQBTrZOOU7YOrEYZa2R7GmLhaVehsVc96aMiv1IENPJn4yhoI4WpKcuck+Qoy2/dPiGHRvP+Syx2r2liUUGJ49J0ZFKbZ0usJA6fYpQyiJddHgCqrHJYwELtW9PAAAAdIXMjNQ+rkc1inQhpevDRe8Fqr4A0N7sKMNM4W86+abmc2C7j0nWD6x68TD1WwDaGwAAAAI4CmOhzwpdq/MGJmBy7Aix2B8eD+RBpXMPe746R7EnKz0Msj81WCfkMcCWop5w+ZJhyn4gjZh+BAFKpgRx9Eh4raqJKa9Er7krTkL82tDuBR08Kq2KHPcBnpt7e05NFU+RizGVKzjnveE/x9J40bNKAAAACxoQWTRN2YuteVp0niJhzusD2oAcdQmuJQZwBkxKepnHHnTrukZoSh8oNUxVxhhphMNg8WnjxVSBpjM8cszKJBIUD0AmrHfCWMs7D0QrZAhmFpvL2h9O+qrdrUb8q1rmvW41u6JiYpFJyJsl/qKRmMJTdZx1qKAAN3wtvaVXXPA+WWNbByTimNUREAD3UOBPsq5K+hm3csQ5vtIUaMfn8vAzyPwGhhicyhgnjc1UR1pAxBqoTeBe8TecdbnhomaFGQXo8K6t2Qr26eGQU1Q50bpUPoICQJUP1seA6BrKJTJJCR6jTccJP9Z5IBKHWoGRF/VwOzXCu4nq8223lA+n09ZUbr39AsrLeNjgo9h7wtfkkoPyBbhoRRy+BpPuZc6DeB7YOCiHQZKbiZziRQQ/2K/Ka90Lwy7wLiJcTwSCnKGBEDh5x5FDZc7E5qK92Eo9djIhD5Cy0zYSAwDF+drM/z4=',
+    verificationMethod: 'did:example:489398593#test',
+  },
+};
+
 async function verifySelectiveDisclosedCredential(derivedProof) {
-  const verified = await verify(derivedProof, {
+  const verified = await verify(temp, {
     suite: new BbsBlsSignatureProof2020(),
     purpose: new purposes.AssertionProofPurpose(),
     documentLoader,
   });
+  console.log(verified);
   return verified;
 }
+
+verifySelectiveDisclosedCredential();
