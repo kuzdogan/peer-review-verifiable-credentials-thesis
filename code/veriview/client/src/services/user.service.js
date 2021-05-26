eb802b4d8d46287ecb0f7c6357d11d3531ed8258
kuzdogan <kaanuzdogan@hotmail.com>
2021-05-26 12:42:12 +0200

    --amend

--- code/veriview/client/src/services/user.service.js
+++ code/veriview/client/src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_PREFIX = 'v1/users';
+const API_PREFIX = '/v1/users';
 
 export const getUserById = (userId) => axios.get(`${API_PREFIX}/${userId}`, { headers: authHeader() }).then((res) => res.data);
