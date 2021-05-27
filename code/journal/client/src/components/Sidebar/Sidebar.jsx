8911826eeb5c84f3ff296f7acab49bc29224460e
kuzdogan <kaanuzdogan@hotmail.com>
2021-05-27 16:17:36 +0200

    Show user id on Sidebar

--- code/journal/client/src/components/Sidebar/Sidebar.jsx
+++ code/journal/client/src/components/Sidebar/Sidebar.jsx
@@ -20,6 +20,7 @@ export default function Navbar() {
         <img className='object-cover w-24 h-24 mx-2 rounded-full' src={userIcon} alt='avatar' />
         <h4 className='mx-2 mt-2 font-medium text-gray-800 hover:underline'>{`${user.firstName} ${user.lastName}`}</h4>
         <p className='mx-2 mt-1 text-sm font-medium text-gray-600 hover:underline'>{user.email}</p>
+        <p className='mx-2 mt-1 text-sm font-medium text-gray-600 hover:underline'>{user.id}</p>
         <p className='mx-2 mt-1 text-sm font-medium text-gray-600 hover:underline'>
           {user.role === 'user' ? 'Author' : 'Editor'}
         </p>
